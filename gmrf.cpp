8d5a8eb3ee2d945cef43141b900873992c98a78c
fishfollower <fishfollower@gmail.com>
2022-11-14 16:16:10 +0000

    added Lauries plosgroup suggestion

--- gmrf.cpp
+++ gmrf.cpp
@@ -1,12 +1,22 @@
 #include <TMB.hpp>
 
+#ifndef REPORT_F
+#define REPORT_F(name,F)					\
+  if(isDouble<Type>::value && F->current_parallel_region<0) {		\
+    Rf_defineVar(Rf_install(#name),					\
+	      PROTECT(asSEXP(name)),F->report);			\
+    UNPROTECT(1);						\
+  }
+#endif
+
+
 template<class Type>
 bool isNA(Type x){
   return R_IsNA(asDouble(x));
 }
 
 template <class Type>
-Type nllBioProcess(array<Type> P, vector<Type> meanVec, vector<Type> logPhi, Type logSdP){
+Type nllBioProcess(array<Type> P, vector<Type> meanVec, vector<Type> logPhi, Type logSdP, objective_function <Type > *of){
     int nrow=P.dim[0];
     int ncol=P.dim[1];
     int n=nrow*ncol;
@@ -22,6 +32,7 @@ Type nllBioProcess(array<Type> P, vector<Type> meanVec, vector<Type> logPhi, Typ
     }
     matrix<Type> Wc(n,n); Wc.setZero();
     matrix<Type> Wd(n,n); Wd.setZero();
+    matrix<Type> Wp(n,n); Wp.setZero();    
     for(int i=0; i<n; ++i){
       for(int j=0; j<n; ++j){
 	if((c(i)==c(j))&&(abs(r(i)-r(j))==1)){
@@ -31,6 +42,12 @@ Type nllBioProcess(array<Type> P, vector<Type> meanVec, vector<Type> logPhi, Typ
  	if( (((r(i)-r(j))==1)&&((c(i)-c(j))==1))||(((r(i)-r(j)==(-1)))&&((c(i)-c(j))==(-1))) ){
        	  Wd(i,j)=1;
 	  Wd(i,i)-=1;
+	}
+	if(logPhi.size()==3){
+          if ((c(i)==(ncol-1)) && (c(j)==(ncol-1)) && (abs(r(i)-r(j))==1) ){
+            Wp(i,j)=1;
+	    Wp(i,i)-=1;
+	  }
 	}
       }
     }
@@ -46,7 +63,10 @@ Type nllBioProcess(array<Type> P, vector<Type> meanVec, vector<Type> logPhi, Typ
     matrix<Type> I(Wc.rows(),Wc.cols());
     I.setIdentity();
     matrix<Type> Q=I-phi(0)*Wc-phi(1)*Wd;
-    
+    if(logPhi.size()==3){
+      Q-=phi(2)*Wp;
+    }
+    REPORT_F(Wp,of)  
     using namespace density;
     return SCALE(GMRF(asSparseMatrix(Q)),exp(logSdP))((P-mP).vec());
 }
@@ -62,7 +82,7 @@ Type objective_function<Type>::operator() ()
   PARAMETER(logSdObs)   
   PARAMETER_ARRAY(P)
 
-  Type jnll = nllBioProcess(P,mu,logPhi,logSdProc);  
+    Type jnll = nllBioProcess(P,mu,logPhi,logSdProc, this);  
 
   for(int i=0; i<Y.dim(0); ++i){
     for(int j=0; j<Y.dim(1); ++j){
@@ -71,5 +91,6 @@ Type objective_function<Type>::operator() ()
       }
     }
   }
+
   return(jnll);
 }
---
+++ plusgp.R
@@ -0,0 +1,33 @@
+library(TMB)
+
+sw<-stockassessment::read.ices("sw.dat")
+
+data<-list(Y=log(sw))
+
+for(i in 1:10)data$Y<-rbind(data$Y, NA) # for predictions
+
+param<-list()
+
+#param$logPhi<-c(0,0) # Only with correlation in cohort and across ages
+param$logPhi<-c(0,0,0) # with additional correlation for plusgroup 
+
+param$mu<-numeric(ncol(data$Y)) # one mean for each age group
+param$logSdProc<-0
+param$logSdObs<-0
+param$P<-matrix(0,nrow=nrow(data$Y), ncol=ncol(data$Y))
+
+compile("gmrf.cpp")
+dyn.load(dynlib("gmrf"))
+
+obj<-MakeADFun(data,param,random="P", DLL="gmrf")
+opt<- nlminb(obj$par,obj$fn, obj$gr)
+sdr<-sdreport(obj)
+pl<-as.list(sdr,"Est")
+plsd<-as.list(sdr,"Std")
+
+matplot(exp(pl$P), type="l")
+matplot(exp(pl$P-2*plsd$P), type="l", add=T)
+matplot(exp(pl$P+2*plsd$P), type="l", add=T)
+
+matplot(exp(data$Y), add=TRUE)
+
